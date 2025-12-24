0f410afc7323e13d31b92614a863dccc9a4619d2
wizardd254 <priyanshk254@gmail.com>
2025-12-24 13:24:41 +0530

    Day7_1

---
+++ 900/make_it_divisble_by_25.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int min_operations(string n, string possible_value)
+{
+    int operations = 0;
+    int checker_index = possible_value.size() - 1;
+
+    for (int i = n.size() - 1; i >= 0; i--)
+    {
+        if (n[i] == possible_value[checker_index])
+        {
+            checker_index--; // Move to the next character in possible_value
+            if (checker_index < 0)
+                break;
+        }
+        else
+            operations++;
+    }
+    if (checker_index >= 0)
+        operations = INT_MAX;
+
+    return operations; // Return the number of operations
+}
+
+int main()
+{
+    long long t; // Number of test cases
+    cin >> t;
+
+    while (t--)
+    {
+        string n; // Input number as a string
+        cin >> n;
+
+        vector<string> possible_values = {"00", "25", "50", "75"};
+        int ans = INT_MAX;
+
+        for (auto possible_value : possible_values)
+            ans = min(ans, min_operations(n, possible_value));
+
+        cout << ans << endl;
+    }
+
+    return 0;
+}
