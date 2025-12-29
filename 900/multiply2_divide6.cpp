374ca4bbc8cf34bb931dcd1a055f9e7eb7592910
wizardd254 <priyanshk254@gmail.com>
2025-12-29 16:16:33 +0530

    Day9-5

---
+++ 900/multiply2_divide6.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long t;
+    cin >> t;
+    while (t--)
+    {
+        long long n;
+        cin >> n;
+        long long count_of_3 = 0;
+        long long count_of_2 = 0;
+
+        while (n > 0 && n % 3 == 0)
+        {
+            count_of_3++;
+            n /= 3;
+        }
+
+        while (n > 0 && n % 2 == 0)
+        {
+            count_of_2++;
+            n /= 2;
+        }
+
+        if (n > 1 || count_of_2 > count_of_3)
+            cout << -1 << endl;
+        else
+
+            cout << count_of_3 + (count_of_3 - count_of_2) << endl;
+    }
+    return 0;
+}
