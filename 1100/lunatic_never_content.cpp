02edacdbda31b47b598caeeacde32219f1ecdc4f
wizardd254 <priyanshk254@gmail.com>
2026-01-21 09:26:57 +0530

    Day22-2

---
+++ 1100/lunatic_never_content.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve()
+{
+    int n;
+
+    cin >> n;
+    vector<int> a(n);
+
+    for (auto &i : a)
+        cin >> i;
+
+    int ans = 0;
+
+    for (int i = 0; i < n; ++i)
+    {
+
+        ans = __gcd(ans, abs(a[i] - a[n - i - 1]));
+    }
+
+    cout << ans << endl;
+}
+
+int main()
+{
+    int t;
+
+    cin >> t;
+    while (t--)
+    {
+
+        solve();
+    }
+}
