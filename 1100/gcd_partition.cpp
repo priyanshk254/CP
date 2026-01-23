0147fde852d001c663b4a3293e9e4dff1c8eeeaa
wizardd254 <priyanshk254@gmail.com>
2026-01-23 15:06:15 +0530

    Day24-3

---
+++ 1100/gcd_partition.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> v(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+
+    ll ans = 0;
+    ll total_sum = accumulate(v.begin(), v.end(), 0LL);
+    ll sum = 0;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        sum += v[i];
+
+        ans = max(ans, __gcd(total_sum - sum, sum));
+    }
+
+    cout << ans << endl;
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr);
+#endif
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
