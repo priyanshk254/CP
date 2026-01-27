4a7dc4093f2e6305430ac9e5f4e2af4c56f9fa98
wizardd254 <priyanshk254@gmail.com>
2026-01-27 18:00:56 +0530

    Day28-3

---
+++ 1200/plus_minus_permutation.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long lcm(long long num1, long long num2)
+{
+
+    return (num1 * num2) / (gcd(num1, num2));
+}
+
+long long calculateSum(long long start, long long end)
+{
+
+    long long sum = ((start + end) * (end - start + 1)) / 2;
+
+    return sum;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        long long n, x, y;
+        cin >> n >> x >> y;
+
+        long long count1 = (n / x) - (n / lcm(x, y));
+
+        long long count2 = (n / y) - (n / lcm(x, y));
+
+        long long ans = calculateSum(n - count1 + 1, n) - calculateSum(1LL, count2);
+
+        cout << ans << endl;
+    }
+}
