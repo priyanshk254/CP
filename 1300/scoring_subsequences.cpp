8fd22f54928d18834d06f3ea66fd37b9656229a1
wizardd254 <priyanshk254@gmail.com>
2026-02-10 20:07:28 +0530

    Day40-2

---
+++ 1300/scoring_subsequences.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<int> a(n);
+
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+
+        vector<int> res;
+
+        for (int i = 0; i < n; i++)
+        {
+            int l = 1, r = i + 1;
+
+            while (l <= r)
+            {
+                int m = (l + r) / 2;
+
+                if (a[i - m + 1] >= m)
+                {
+                    l = m + 1;
+                }
+                else
+                {
+                    r = m - 1;
+                }
+            }
+            res.push_back(r);
+        }
+
+        for (auto i : res)
+        {
+            cout << i << " ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
