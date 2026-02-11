78478a22f6636f553a4e7fe8ce39804515aafb3b
wizardd254 <priyanshk254@gmail.com>
2026-02-11 11:11:25 +0530

    Day41-1

---
+++ 1300/yet-another-problem-about-pair-satisfying-inequality.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    long long res = 0;
+    vector<int> v;
+    for (int i = 0; i < n; i++)
+    {
+
+        if (a[i] >= i + 1)
+        {
+            continue;
+        }
+
+        res += (long long)(lower_bound(v.begin(), v.end(), a[i]) - v.begin());
+
+        v.push_back(i + 1);
+    }
+    cout << res << '\n';
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 1; i <= t; i++)
+        solve();
+    return 0;
+}
